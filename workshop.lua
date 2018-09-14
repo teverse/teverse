4cc884d81732adb5981a4722467f09fdf8a6e6d6
Neztore <josh@muir.xyz>
2018-09-15 00:11:52 +0100

    [WIP] Added new block button
    Needs cleanup & Fix. Doesn't currently work.

--- workshop.lua
+++ workshop.lua
@@ -8,3 +8,19 @@ menuBarTop.parent = workshop.interface
 
 local menuFile = menuBarTop:createItem("File")
 local menuNew = menuFile:createItem("New")
+local makeBlock = menuFile:createItem("New block")
+
+-- Block creation function. Creates a new block and positions it relative to the user's camera.
+makeBlock:mouseLeftPressed(function ()
+	local newBlock = engine.block("block")
+	newBlock.parent = workspace
+
+	local camera = workspace.camera
+	local cameraPos = camera.position
+	-- not sure if this how to get look vector
+	local lookVector = camera.rotation * vector3(0, 0, 1)
+	-- absolutely no idea where this positions it.
+	local addVector = vector3(2, 0, 0)
+	newBlock.position = lookVector + addVector
+
+end)
