d0084dbba84937042e16e213a299e74104ac86d1
teverse <43248871+teverse@users.noreply.github.com>
2018-09-15 08:54:02 +0100

    New menu options

--- workshop.lua
+++ workshop.lua
@@ -7,19 +7,25 @@ menuBarTop.position = guiCoord(0, 0, 0, 0)
 menuBarTop.parent = workshop.interface
 
 local menuFile = menuBarTop:createItem("File")
-local menuFileNew = menuFile:createItem("New")
+local menuFileNew = menuFile:createItem("New Scene")
+local menuFileOpen = menuFile:createItem("Open Scene")
+local menuFileSave = menuFile:createItem("Save Scene")
+local menuFileSaveAs = menuFile:createItem("Save Scene As")
+
+local menuEdit = menuBarTop:createItem("Edit")
+local menuEditUndo = menuEdit:createItem("Undo")
+local menuEditRedo = menuEdit:createItem("Redo")
 
 local menuInsert = menuBarTop:createItem("Insert")
-local makeBlock = menuInsert:createItem("Block")
+local menuInsertBlock = menuInsert:createItem("Block")
 
--- Block creation function. Creates a new block and positions it relative to the user's camera.
-makeBlock:mouseLeftPressed(function ()
+-- Block creation function. Creates a new block and positions it relative to the user's camera
+menuInsertBlock:mouseLeftPressed(function ()
 	local newBlock = engine.block("block")
 	newBlock.parent = workspace
 
 	local camera = workspace.camera
-
+		
 	local lookVector = camera.rotation * vector3(0, 0, 1)
 	newBlock.position = camera.position + (lookVector * 10)
-
 end)
