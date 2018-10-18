eab7736c77b72b94021c5f8394dfb31937bde591
joritochip <jojobuddyr@gmail.com>
2018-10-18 13:00:49 -0600

    Fix UI, clean up camera/selection, comments, bugs fixed (#7)
    * Fix UI, clean up camera and selection
    
    Made camera code less bulky by using an array, moved selection code to proper place preventing the need for a redraw variable
    
    * Added functionality to selected items label
    
    * Added comments/fixed capitalization
    
    Fixed capitalization in a few spots and added some more clarification for the variables at the beginning.
    
    * Fixed selection code
    
    Noticed that if LeftShift is being held and the user clicks on a part to deselect it, it deselects but immediately reselects. Moved line to select mouseHit to the correct spot.

--- workshop.lua
+++ workshop.lua
@@ -9,9 +9,9 @@
 -- 
 
 local history = {}
-local dirty = {} -- record things that have changed since last action
-local currentPoint = 0 -- the current point in history used to undo 
-local goingBack = false
+local dirty = {} -- Records changes made since last action
+local currentPoint = 0 -- The current point in the history array that is used to undo
+local goingBack = false -- Used to prevent objectChanged from functioning while undoing
 
 local function objectChanged(property)
 	-- TODO: self is a reference to an event object
@@ -33,10 +33,6 @@ local function savePoint()
 	local newPoint = {}
 	
 	for object, properties in pairs(dirty) do
-		--local thisObject = {}
-		--for property, oldValue in pairs(properties) do
-		--	table.insert(thi
-		--end
 		newPoint[object] = properties
 	end
 	
@@ -67,21 +63,25 @@ workspace:childAdded(function(child)
 end)
 
 function undo()
+	if currentPoint == 0 then return end
+	
 	currentPoint = currentPoint - 1
 	local snapShot = history[currentPoint] 
 	if not snapShot then snapShot = {} end
 
 	goingBack = true
+	
 	for object, properties in pairs(snapShot) do
 		for property, value in pairs(properties) do
 			object[property] = value
 		end
 	end
+	
 	goingBack = false
 end
 
 function redo()
-	if currentpoint >= #history then
+	if currentPoint >= #history then
 		return print("Debug: can't redo.")
 	end
 
@@ -90,61 +90,67 @@ function redo()
 	if not snapShot then return print("Debug: no snapshot found") end
 
 	goingBack = true
+	
 	for object, properties in pairs(snapShot) do
 		for property, value in pairs(properties) do
 			object[property] = value
 		end
 	end
+	
 	goingBack = false
 end
 
- -- 
- -- UI
- --
+-- 
+-- UI
+--
+ 
+-- Menu Bar Creation
 
 local menuBarTop = engine.guiMenuBar()
 menuBarTop.size = guiCoord(1, 0, 0, 24)
 menuBarTop.position = guiCoord(0, 0, 0, 0)
 menuBarTop.parent = engine.workshop.interface
 
+-- File Menu
+
 local menuFile = menuBarTop:createItem("File")
+
 local menuFileNew = menuFile:createItem("New Scene")
 local menuFileOpen = menuFile:createItem("Open Scene")
 local menuFileSave = menuFile:createItem("Save Scene")
 local menuFileSaveAs = menuFile:createItem("Save Scene As")
 
+-- Edit Menu
+
 local menuEdit = menuBarTop:createItem("Edit")
 local menuEditUndo = menuEdit:createItem("Undo")
 local menuEditRedo = menuEdit:createItem("Redo")
 
+-- Insert Menu
+
 local menuInsert = menuBarTop:createItem("Insert")
 local menuInsertBlock = menuInsert:createItem("Block")
 
--- Define events to handle undo/redo button clicks
 menuEditUndo:mouseLeftPressed(undo)
 menuEditRedo:mouseLeftPressed(redo)
-
--- Define place loading events
-menuFileOpen:mouseLeftPressed(function()
-	-- Tell the Workshop APIs to initate a game load.
-	engine.workshop:openFileDialogue()
-end)
 
 menuFileNew:mouseLeftPressed(function()
 	engine.workshop:newGame()
 end)
 
 menuFileOpen:mouseLeftPressed(function()
+	-- Tell the Workshop APIs to initate a game load.
+	engine.workshop:openFileDialogue()
+end)
+
+menuFileSave:mouseLeftPressed(function()
 	engine.workshop:saveGame() -- returns boolean
 end)
 
-menuFileOpen:mouseLeftPressed(function()
+menuFileSaveAs:mouseLeftPressed(function()
 	engine.workshop:saveGameAsDialogue()
 end)
 
-
-
--- Block creation function. Creates a new block and positions it relative to the user's camera
 menuInsertBlock:mouseLeftPressed(function ()
 	local newBlock = engine.block("block")
 	newBlock.colour = colour(1,0,0)
@@ -158,6 +164,9 @@ menuInsertBlock:mouseLeftPressed(function ()
 
 	savePoint() -- for undo/redo
 end)
+
+-- Properties Window
+
 windowProperties = engine.guiWindow()
 windowProperties.size = guiCoord(0, 220, 0.5, -12)
 windowProperties.position = guiCoord(1, -220, 0, 24)
@@ -166,20 +175,20 @@ windowProperties.text = "Properties"
 windowProperties.fontSize = 10
 windowProperties.fontFile = "OpenSans-Regular"
 
+-- Selected Integer Text
+
 local txtProperty = engine.guiTextBox()
 txtProperty.size = guiCoord(1, -50, 0, 50)
 txtProperty.position = guiCoord(0, 0, 0, 0)
 txtProperty.fontSize = 9
 txtProperty.fontFile = "OpenSans-Regular"
-txtProperty.text = "0 Selected Items"
+txtProperty.text = "0 items selected"
 txtProperty.parent = windowProperties
 txtProperty.textColour = colour(1,0,0)
 
-
-
 -- 
--- Workshop camera
--- Currently modified from the Wiki Tutorial
+-- Workshop Camera
+-- Altered from https://wiki.teverse.com/tutorials/base-camera
 --
 
 -- The distance the camera is from the target
@@ -187,9 +196,9 @@ local target = vector3(0,0,0) -- A virtual point that the camera
 local currentDistance = 20
 
 -- The amount the camera moves when you use the scrollwheel
-local zoomStep     = 3
-local rotateStep   = -0.0045
-local moveStep     = 0.5 -- how fast the camera moves
+local zoomStep = 3
+local rotateStep = -0.0045
+local moveStep = 0.5 -- how fast the camera moves
 
 local camera = workspace.camera
 
@@ -197,8 +206,20 @@ local camera = workspace.camera
 camera.position = target - vector3(0, -5, currentDistance)
 camera:lookAt(target)
 
+-- Camera key input values
+local cameraKeyEventLooping = false
+local cameraKeyArray = {
+	[enum.key.w] = vector3(0, 0, -1),
+	[enum.key.s] = vector3(0, 0, 1),
+	[enum.key.a] = vector3(-1, 0, 0),
+	[enum.key.d] = vector3(1, 0, 0),
+	[enum.key.q] = vector3(0, -1, 0),
+	[enum.key.e] = vector3(0, 1, 0)
+}
+
 local function updatePosition()
 	local lookVector = camera.rotation * vector3(0, 0, 1)
+	
 	camera.position = target + (lookVector * currentDistance)
 	camera:lookAt(target)
 end
@@ -210,54 +231,31 @@ end)
 
 engine.input:mouseMoved(function( input )
 	if engine.input:isMouseButtonDown( enums.mouseButton.right ) then
-
 		local pitch = quaternion():setEuler(input.movement.y * rotateStep, 0, 0)
 		local yaw = quaternion():setEuler(0, input.movement.x * rotateStep, 0)
 
 		-- Applied seperately to avoid camera flipping on the wrong axis.
 		camera.rotation = yaw * camera.rotation;
 		camera.rotation = camera.rotation * pitch
+		
 		--updatePosition()
 	end
 end)
 
-
-local cameraKeyEventLooping = false
-
--- These key events trigger a loop that will continuously move the camera until all keys are released.
 engine.input:keyPressed(function( inputObj )
-
-    if inputObj.key == enums.key.w or
-       inputObj.key == enums.key.a or
-       inputObj.key == enums.key.s or
-       inputObj.key == enums.key.d or
-       inputObj.key == enums.key.q or
-       inputObj.key == enums.key.e and 
-       not cameraKeyEventLooping then
-
-    	cameraKeyEventLooping = true
-
-    	repeat
+	if cameraKeyArray[inputObj.key] and (not cameraKeyEventLooping) then
+		cameraKeyEventLooping = true
+		
+		repeat
 			local cameraPos = camera.position
-			if engine.input:isKeyDown(enums.key.w) then
-				cameraPos = cameraPos - (camera.rotation * vector3(0, 0, 1) * moveStep)
-			elseif engine.input:isKeyDown(enums.key.s) then
-				cameraPos = cameraPos + (camera.rotation * vector3(0, 0, 1) * moveStep)
-			elseif engine.input:isKeyDown(enums.key.a) then
-				cameraPos = cameraPos - (camera.rotation * vector3(1, 0, 0) * moveStep)
-			elseif engine.input:isKeyDown(enums.key.d) then
-				cameraPos = cameraPos + (camera.rotation * vector3(1, 0, 0) * moveStep)
-			elseif engine.input:isKeyDown(enums.key.q) then
-				cameraPos = cameraPos - (camera.rotation * vector3(0, 1, 0) * moveStep)
-			elseif engine.input:isKeyDown(enums.key.e) then
-				cameraPos = cameraPos + (camera.rotation * vector3(0, 1, 0) * moveStep)
-			end
-
-			cameraKeyEventLooping = (cameraPos ~= camera.position) -- If there's no keys down, stop the loop!
+			
+			cameraPos = cameraPos + (camera.rotation * cameraKeyArray[inputObj.key] * moveStep)
+			cameraKeyEventLooping = (cameraPos ~= camera.position)
 			camera.position = cameraPos
-
+				
 			wait(0.001)
-		until not cameraKeyEventLooping
+		until
+			not cameraKeyEventLooping
 	end
 end)
 
@@ -293,9 +291,7 @@ outlineSelectedBlock.opacity = 0
 
 
 local selectedItems = {}
-local redraw = true -- a way to stop updating the selection box when nothing has happened.
 
---WIP
 engine.graphics:frameDrawn(function()	
 	local mouseHit = engine.physics:rayTestScreen( engine.input.mousePosition ) -- accepts vector2 or number,number
 	if mouseHit then 
@@ -304,33 +300,6 @@ engine.graphics:frameDrawn(function()
 		outlineHoverBlock.opacity = 1
 	else
 		outlineHoverBlock.opacity = 0
-	end
-
-	if #selectedItems > 0 and redraw then
-		outlineSelectedBlock.opacity = 1
-		
-		-- used to calculate bounding box area...
-		local upper = selectedItems[1].position + (selectedItems[1].size/2)
-		local lower = selectedItems[1].position - (selectedItems[1].size/2)
-
-		for i, v in pairs(selectedItems) do
-			local topLeft = v.position + (v.size/2)
-			local btmRight = v.position - (v.size/2)
-			
-		
-			upper.x = math.max(topLeft.x, upper.x)
-			upper.y = math.max(topLeft.y, upper.y)
-			upper.z = math.max(topLeft.z, upper.z)
-
-			lower.x = math.min(btmRight.x, lower.x)
-			lower.y = math.min(btmRight.y, lower.y)
-			lower.z = math.min(btmRight.z, lower.z)
-		end
-
-		outlineSelectedBlock.position = (upper+lower)/2
-		outlineSelectedBlock.size = upper-lower
-	elseif #selectedItems == 0 then
-		outlineSelectedBlock.opacity = 0
 	end
 end)
 
@@ -345,6 +314,7 @@ engine.input:mouseLeftPressed(function( input )
 	if not engine.input:isKeyDown(enums.key.leftShift) then
 		-- deselect everything and move on
 		selectedItems = {}
+		table.insert(selectedItems, mouseHit)
 	else
 		for i,v in pairs(selectedItems) do
 			if v == mouseHit then
@@ -354,7 +324,37 @@ engine.input:mouseLeftPressed(function( input )
 			end
 		end
 	end
+		
+	if #selectedItems > 1 then
+		outlineSelectedBlock.opacity = 1
+		
+		-- used to calculate bounding box area...
+		local upper = selectedItems[1].position + (selectedItems[1].size/2)
+		local lower = selectedItems[1].position - (selectedItems[1].size/2)
 
-	table.insert(selectedItems, mouseHit)
+		for i, v in pairs(selectedItems) do
+			local topLeft = v.position + (v.size/2)
+			local btmRight = v.position - (v.size/2)
+		
+			upper.x = math.max(topLeft.x, upper.x)
+			upper.y = math.max(topLeft.y, upper.y)
+			upper.z = math.max(topLeft.z, upper.z)
+
+			lower.x = math.min(btmRight.x, lower.x)
+			lower.y = math.min(btmRight.y, lower.y)
+			lower.z = math.min(btmRight.z, lower.z)
+		end
+
+		outlineSelectedBlock.position = (upper+lower)/2
+		outlineSelectedBlock.size = upper-lower
+	elseif #selectedItems == 1 then
+		outlineSelectedBlock.opacity = 1
+		outlineSelectedBlock.position = selectedItems[1].position
+		outlineSelectedBlock.position = selectedItems[1].size
+	elseif #selectedItems == 0 then
+		outlineSelectedBlock.opacity = 0
+	end
+		
+	txtProperty.text = #selectedItems .. " item" + (#selectedItems == 1 and "" or "s") + " selected"
 end)
 
