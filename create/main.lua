ed44d1091f1847ed232fde8b90f8bd5a0f41dda6
teverse <43248871+teverse@users.noreply.github.com>
2019-04-27 10:53:13 +0100

    Update main.lua

--- create/main.lua
+++ create/main.lua
@@ -1,1 +1,8 @@
-local themer = require(“”)
+local controllers = {
+	theme   = require("tevcore:create/controllers/theme.lua"),
+	ui      = require("tevcore:create/controllers/ui.lua"),
+	camera  = require("tevcore:create/controllers/camera.lua"),
+	console = require("tevcore:create/controllers/console.lua"),
+	tool    = require("tevcore:create/controllers/tool.lua")
+}
+
