9c9786d393d3698e8fd9c059d43823d355249174
teverse <43248871+teverse@users.noreply.github.com>
2019-04-28 11:23:19 +0100

    Update ui.lua

--- create/controllers/ui.lua
+++ create/controllers/ui.lua
@@ -3,14 +3,17 @@
 local uiController = {}
 local themeController = require("tevcore:create/controllers/theme.lua")
 
-uiController.createFrame = function(parent, properties)
+uiController.createFrame = function(parent, properties, style)
     local gui = engine.construct("guiFrame", parent, properties)
-	themeController.addGUI(gui, "primary")
+	themeController.addGUI(gui, style and style or "primary")
 	return gui
 end
 
 uiController.createMainInterface = function()
-
+    local sideBar = uiController.createFrame(nil, {
+        size = guiCoord(0,40,1,0),
+        position = guiCoord(0,0,0,0)
+    })
 end
 
 return uiController
