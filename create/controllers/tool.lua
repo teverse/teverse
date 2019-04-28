43ed01a15314c05f3ef81653860b6be15237865a
teverse <43248871+teverse@users.noreply.github.com>
2019-04-28 20:14:42 +0100

    Update tool.lua

---
+++ create/controllers/tool.lua
@@ -0,0 +1,11 @@
+-- Copyright 2019 teverse.com
+
+local toolsController = {}
+
+toolsController.container = nil
+
+toolsController.add = function(toolName, toolIcon, toolDesc, ...)
+
+end
+
+return toolsController
