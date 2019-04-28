f9416c023a47f75bf7159e68f9bb8066c2030b25
teverse <43248871+teverse@users.noreply.github.com>
2019-04-28 11:35:52 +0100

    Update theme.lua

--- create/controllers/theme.lua
+++ create/controllers/theme.lua
@@ -2,16 +2,22 @@
 
 local themeController = {}
 
+themeController.currentTheme = {}
 themeController.guis = {} --make this a weak metatable (keys)
 
 themeController.setTheme = function(theme)
     
 end)
 
+themeController.applyTheme = function(gui)
+local style = themeController.guis[gui]
+end
+
 themeController.add = function(gui, style)
     if themeController.guis[gui] then return end
     
     themeController.guis[gui] = style
+themeController.applyTheme(gui)
 end
 
 return themeController
