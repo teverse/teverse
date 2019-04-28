d0026711192831e83050fb7f2946fa06591489e0
teverse <43248871+teverse@users.noreply.github.com>
2019-04-28 11:21:21 +0100

    Update ui.lua

--- create/controllers/ui.lua
+++ create/controllers/ui.lua
@@ -5,8 +5,8 @@ local themeController = require("tevcore:create/controllers/theme.lua")
 
 uiController.createFrame = function(parent, properties)
     local gui = engine.construct("guiFrame", parent, properties)
-		themeController.addGUI(gui, "primary")
-		return gui
+	themeController.addGUI(gui, "primary")
+	return gui
 end
 
 uiController.createMainInterface = function()
