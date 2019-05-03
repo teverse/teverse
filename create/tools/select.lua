ea311edeacab449fb6c126ad58b755605466970b
teverse <43248871+teverse@users.noreply.github.com>
2019-05-03 17:28:12 +0100

    Update select.lua

--- create/tools/select.lua
+++ create/tools/select.lua
@@ -1,4 +1,17 @@
 -- Copyright 2019 Teverse
 -- Select tool
 
+local toolName = "Select"
+local toolIcon = "fa:s-times"
+local toolDesc = "Use this to select and move primitives."
+
+local toolActivated = function(id)
+  
+end
+
+local toolDeactivated = function(id)
+  
+end
+
 local toolController = require("tevgit:create/controllers/tool.lua")
+return toolController.add(toolName, toolIcon, toolDesc, toolActivated, toolDeactivated)
