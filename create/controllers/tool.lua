66ce7ad3fd6b9c91f32059c9f633d3d78cb14b07
teverse <43248871+teverse@users.noreply.github.com>
2019-05-03 17:27:58 +0100

    Update tool.lua

--- create/controllers/tool.lua
+++ create/controllers/tool.lua
@@ -6,12 +6,28 @@ local toolsController = {}
 toolsController.container = nil
 toolsController.ui = nil
 
-toolsController.add = function(toolName, toolIcon, toolDesc, ...)
-    local button = toolsController.ui.create("guiImage", 
-                                        toolsController.container, 
-                                        {size=guiCoord(0, 40, 0, 40)},
-                                        "main")
+toolsController.currentTool = 0
+toolsController.tools = {}
 
+-- "data" is a table that can be read and written to by activation and deactivation functions
+-- these functions should access it via toolsController.tools[id].data
+toolsController.add = function(toolName, toolIcon, toolDesc, toolActivated, toolDeactivated, data)
+    local toolId = #toolsController.tools + 1
+    local button = toolsController.ui.create("guiImage", 
+                                             toolsController.container, 
+                                             {   
+                                                 size = guiCoord(0, 40, 0, 40),
+                                                 position = guiCoord(0, 0, 0, 5 + (30 * #toolsController.tools)),
+                                                 guiStyle = enums.guiStyle.noBackground,
+                                                 texture = toolIcon
+                                             },
+                                             "main")
+
+    toolsController.tools:insert({id = toolId, 
+                                  gui = button, 
+                                  data = data and data or {},
+                                  activate = toolActivated, 
+                                  deactivate=toolDeactivated})
 
 end
 
