3050844efb246202a169e088e6ffa250e5dea934
TheFluffyCat <43320783+Mooshua@users.noreply.github.com>
2019-05-07 09:53:48 -0700

    Create Quicksilver
    A small little function that cleans up property assignment after creating calls with basic built-in error handling

---
+++ Quicksilver.lua
@@ -0,0 +1,24 @@
+--[[
+  (C) 2019 TheFluffyCat / Mooshua (Github / Teverse)
+  
+  Quicksilver.lua
+  
+  Small little function that cleans up create calls. 
+  Input the instance with a table like property = value.
+  It will edit the properties to match the table.
+]]
+ 
+function silver(int,tab)
+ 	for property,value in pairs(tab) do
+		local success, err = pcall(function() int[property] = value end)
+		if success == false then
+			if int[property] == nil then
+				error("[ERROR] Quicksilver: Bad Input? Details: "..err,2)
+			else
+				error("[ERROR] Quicksilver: Unknown Error. Details: "..err,1)
+			end
+		end
+	end
+end
+
+return silver
