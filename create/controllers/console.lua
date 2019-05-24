47bc3dfdde507d89c0a3c5b93d7a1651b3d14591
joritochip <jojobuddyr@gmail.com>
2019-05-24 09:43:03 -0600

    Fixed semicolons
    lol

--- create/controllers/console.lua
+++ create/controllers/console.lua
@@ -22,83 +22,83 @@ function stringSplit(inputStr, sep)
 end
 
 local windowObject = uiController.create("guiFrame", engine.workshop.interface, {
-    name = "outputConsole";
-    visible = false;
-    size = guiCoord(0.5, 0, 0.5, 0);
-    position = guiCoord(0.25, 0, 0.25, 0);
+    name = "outputConsole",
+    visible = false,
+    size = guiCoord(0.5, 0, 0.5, 0),
+    position = guiCoord(0.25, 0, 0.25, 0)
 })
 
 consoleController.consoleObject = windowObject
 
 local topbar = uiController.create("guiFrame", windowObject, {
-    size = guiCoord(1, 0, 0, 25);
-    name = "topbar";
+    size = guiCoord(1, 0, 0, 25),
+    name = "topbar"
 }, "primary")
 
 local titleText = uiController.create("guiTextBox", topbar, {
-    size = guiCoord(0.2, 0, 1, -10);
-    position = guiCoord(0, 10, 0, 5);
-    text = "Console";
-    fontSize = 20;
-    readOnly = true;
-    name = "windowTitle";
+    size = guiCoord(0.2, 0, 1, -10),
+    position = guiCoord(0, 10, 0, 5),
+    text = "Console",
+    fontSize = 20,
+    readOnly = true,
+    name = "windowTitle"
 }, "primary")
 
 local closeButton = uiController.create("guiTextBox", topbar, {
-    backgroundColour  = colour:fromRGB(255, 0, 0);
-    text = "X";
-    size = guiCoord(0, 25, 0, 25);
-    align = enums.align.middle;
-    position = guiCoord(1, -25, 0, 0);
-    name = "closeButton";
-    readOnly = true;
+    backgroundColour  = colour:fromRGB(255, 0, 0),
+    text = "X",
+    size = guiCoord(0, 25, 0, 25),
+    align = enums.align.middle,
+    position = guiCoord(1, -25, 0, 0),
+    name = "closeButton",
+    readOnly = true
 }, "primary")
 
 local scrollView = uiController.create("guiScrollView", windowObject, {
-    size = guiCoord(1, 0, 1, -50);
-    position = guiCoord(0, 0, 0, 25);
-    canvasSize = guiCoord(1, 0, 0, 0);
+    size = guiCoord(1, 0, 1, -50),
+    position = guiCoord(0, 0, 0, 25),
+    canvasSize = guiCoord(1, 0, 0, 0)
 })
 
 local entryLabel = uiController.create("guiTextBox", scrollView, {
-	size = guiCoord(1, -10, 0, 50);
-	position = guiCoord(0, 0, 0, 0);
-	name = "entryLabel";
-	wrap = true;
-	multiline = true;
-	readOnly = true;
-	align = enums.align.topLeft;
-	fontSize = 20;
-	textColour = colour(1, 1, 1);
+	size = guiCoord(1, -10, 0, 50),
+	position = guiCoord(0, 0, 0, 0),
+	name = "entryLabel",
+	wrap = true,
+	multiline = true,
+	readOnly = true,
+	align = enums.align.topLeft,
+	fontSize = 20,
+	textColour = colour(1, 1, 1)
 }, "default")
 
 local cmdInput = uiController.create("guiFrame", windowObject, {
-	size = guiCoord(1, 0, 0, 25);
-	position = guiCoord(0, 0, 1, -25);
-	name = "cmdInput";
+	size = guiCoord(1, 0, 0, 25),
+	position = guiCoord(0, 0, 1, -25),
+	name = "cmdInput"
 }, "secondary")
 
 local cmdDecorText = uiController.create("guiTextBox", cmdInput, {
-	size = guiCoord(0, 20, 1, 0);
-	position = guiCoord(0, 5, 0, 0);
-	readOnly = true;
-	multiline = false;
-	text = ">";
-	align = enums.align.middle;
-	fontSize = 20;
-	textColour = colour(1, 1, 1);
-	name = "cmdDecorText";
+	size = guiCoord(0, 20, 1, 0),
+	position = guiCoord(0, 5, 0, 0),
+	readOnly = true,
+	multiline = false,
+	text = ">",
+	align = enums.align.middle,
+	fontSize = 20,
+	textColour = colour(1, 1, 1),
+	name = "cmdDecorText"
 }, "secondary")
 
 local cmdInputText = uiController.create("guiTextBox", cmdInput, {
-	size = guiCoord(1, -30, 1, 0);
-	position = guiCoord(0, 25, 0, 0);
-	multiline = false;
-	text = "Type a command";
-	align = enums.align.middleLeft;
-	fontSize = 20;
-	textColour = colour(1, 1, 1);
-	name = "cmdInputText";
+	size = guiCoord(1, -30, 1, 0),
+	position = guiCoord(0, 25, 0, 0),
+	multiline = false,
+	text = "Type a command",
+	align = enums.align.middleLeft,
+	fontSize = 20,
+	textColour = colour(1, 1, 1),
+	name = "cmdInputText"
 }, "secondary")
 
 closeButton:mouseLeftPressed(function() 
