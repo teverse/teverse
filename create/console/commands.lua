36e7c45d8c827f8db41e7f31f129028e3038c9a9
joritochip <jojobuddyr@gmail.com>
2019-05-24 12:26:20 -0600

    Add files via upload

--- create/console/commands.lua
+++ create/console/commands.lua
@@ -1,29 +1,33 @@
-local consoleController = require("tevgit:create/controllers/console.lua")
+local consoleCommands = {}
 
-return  {
-    echo = {
-        commands = {"echo", "repeat"}, -- Allows for aliases
-        arguments = {"textToEcho"},
-        description = "Echoes the given text back to you in the console - Used as a test command",
-        execute = function(args)
-            print(args[1])
-        end
-    },
-	exe = {
-		commands = {"loadstring", "execute"},
-		arguments = {"code"},
-		description = "Executes the given code in the environment",
-		execute = fucntion(args)
-			engine.workshop:loadString(args)
-		end
-	},
-	clear = {
-		commands = {"clear"},
-		arguments = {},
-		description = "Clears all entries from the output",
-		execute = function(args)
-			consoleController.outputLines = {}
-			print("Cleared console successfully!")
-		end
+consoleCommands.getCommands = function(consoleController)
+	return {
+		echo = {
+			commands = {"echo", "repeat"}, -- Allows for aliases
+			arguments = {"text"},
+			description = "Echoes the given text back to you in the console",
+			execute = function(args)
+				print(args[1])
+			end
+		},
+		exe = {
+			commands = {"loadstring", "execute"},
+			arguments = {"code"},
+			description = "Executes the given code in the environment",
+			execute = function(args)
+				engine.workshop:loadString(args)
+			end
+		},
+		clear = {
+			commands = {"clear"},
+			arguments = {},
+			description = "Clears all entries from the output",
+			execute = function(args)
+				consoleController.outputLines = {}
+				print("Cleared console successfully!")
+			end
+		}
 	}
-}
+end
+
+return consoleCommands
