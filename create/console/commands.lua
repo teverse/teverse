4c1770a9e69f906d949b8611b56e77a5d9559935
joritochip <jojobuddyr@gmail.com>
2019-05-24 10:21:12 -0600

    Add files via upload

--- create/console/commands.lua
+++ create/console/commands.lua
@@ -1,10 +1,29 @@
+local consoleController = require("tevgit:create/controllers/console.lua")
+
 return  {
     echo = {
-        commands = {"echo", "repeat"}; -- Allows for aliases
-        arguments = {"textToEcho"};
-        description = "Echoes the given text back to you in the console - Used as a test command";
+        commands = {"echo", "repeat"}, -- Allows for aliases
+        arguments = {"textToEcho"},
+        description = "Echoes the given text back to you in the console - Used as a test command",
         execute = function(args)
-            print(args[1]) --// prints the contents of the first argument
-        end;
-    };
+            print(args[1])
+        end
+    },
+	exe = {
+		commands = {"loadstring", "execute"},
+		arguments = {"code"},
+		description = "Executes the given code in the environment",
+		execute = fucntion(args)
+			engine.workshop:loadString(args)
+		end
+	},
+	clear = {
+		commands = {"clear"},
+		arguments = {},
+		description = "Clears all entries from the output",
+		execute = function(args)
+			consoleController.outputLines = {}
+			print("Cleared console successfully!")
+		end
+	}
 }
