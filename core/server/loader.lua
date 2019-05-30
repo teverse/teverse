268f9da0fe974c7ede9e4cb3391aabea0bbd623b
teverse <jay@teverse.com>
2019-05-30 15:41:56 +0100

    bug fixes

--- core/server/loader.lua
+++ core/server/loader.lua
@@ -7,3 +7,17 @@
 
 local chat = require("tevgit:core/server/chat.lua")
 local char = require("tevgit:core/server/characterController.lua")
+
+-- Purely for testing purposes only:
+while wait(1) do
+	local blocks = {}
+	for i = 1,15 do
+		local block = engine.construct("block", workspace, {size = vector3(1,2,1), position = vector3(0,15,0), static=false, colour = colour(0,0,1)})
+		table.insert(blocks, block)
+		wait()
+	end
+	wait(5)
+	for _,v in pairs(blocks) do
+		v:destroy()
+	end
+end
