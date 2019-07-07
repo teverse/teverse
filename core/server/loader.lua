8b6c782b8a92fd8f1225c83e49643ebcd59a9271
teverse <jay@teverse.com>
2019-07-07 22:05:57 +0100

    test

--- core/server/loader.lua
+++ core/server/loader.lua
@@ -9,18 +9,7 @@ local chat = require("tevgit:core/server/chat.lua")
 local char = require("tevgit:core/server/characterController.lua")
 
 -- Purely for testing purposes only:
-while wait(1) do
-	local blocks = {}
-	--[[for x = 1, 5 do
-		for i = 1,5 do
-			local block = engine.construct("block", workspace, {size = vector3(1,1,1), position = vector3(x,i,0), static=false, colour = colour(math.random(),math.random(),math.random())})
-			table.insert(blocks, block)
-		end
-		wait()
-	end]]
-	local block = engine.construct("block", workspace, {size = vector3(1,1,1), position = vector3(0,15,0), static=false, colour = colour(math.random(),math.random(),math.random())})
-	table.insert(blocks, block)
-	wait(4)
-
-	for _,v in pairs(blocks) do v:destroy() end
+local block = engine.construct("block", workspace, {size = vector3(1,1,1), position = vector3(0,15,0), static=false, colour = colour(math.random(),math.random(),math.random())})
+while wait(5) do
+	block.position = vector3(0,15,0)
 end
