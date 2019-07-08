f014ff4e89cb3dcb64a0b47fce423a09847fe0df
teverse <jay@teverse.com>
2019-07-08 16:15:36 +0100

    test

--- core/server/loader.lua
+++ core/server/loader.lua
@@ -9,10 +9,6 @@ local chat = require("tevgit:core/server/chat.lua")
 local char = require("tevgit:core/server/characterController.lua")
 
 -- Purely for testing purposes only:
-while wait(.1) do
-	spawnThread(function()
+while wait(.8) do
 		local block = engine.construct("block", workspace, {size = vector3(1,1,1), position = vector3(0,15,0), static=false, colour = colour(math.random(),math.random(),math.random())})
-		wait(5)
-		block:destroy()
-	end)
 end
