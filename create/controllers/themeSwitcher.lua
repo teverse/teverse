b85062eef127746635e110364049240562b99492
xxtom00 <34427163+xxtom00@users.noreply.github.com>
2019-07-28 13:55:10 +0100

    fix

--- create/controllers/themeSwitcher.lua
+++ create/controllers/themeSwitcher.lua
@@ -2,7 +2,6 @@ local controller = {}
 
 local uiController = require("tevgit:create/controllers/ui.lua")
 local themeController = require("tevgit:create/controllers/theme.lua")
-local dockController = require("tevgit:create/controllers/dock.lua")
 
 controller.window = nil
 controller.workshop = nil
@@ -13,44 +12,44 @@ function controller.createUI(workshop)
     controller.window = uiController.createWindow(workshop.interface, guiCoord(1, -15, 1, -50), guiCoord(0, 250, 0, 40), "Theme")
     controller.window.visible = true
 
-    local darkBtn = uiController.create("guiButton", controller.content, {
+    local darkBtn = engine.construct("guiFrame", controller.window.content, {
         name = "input",
         size = guiCoord(0, 5, 0, 5),
         position = guiCoord(0, 5, 0, 5),
         text = "",
         backgroundAlpha = 0.75,
-        backgroundColour = colour:fromRGB(66, 66, 76),
+        backgroundColour = colour:fromRGB(66, 66, 76)
     })
 
-    local lightBtn = uiController.create("guiButton", controller.content, {
+    local lightBtn = engine.construct("guiFrame", controller.window.content, {
         name = "input",
         size = guiCoord(0, 5, 0, 5),
         position = guiCoord(0, 15, 0, 5),
         text = "",
         backgroundAlpha = 0.75,
-        backgroundColour = colour:fromRGB0,0,0),
+        backgroundColour = colour:fromRGB(0,0,0)
     })
 
-    local moreBtn = uiController.create("guiButton", controller.content, {
+    local moreBtn = engine.construct("guiFrame", controller.window.content, {
         name = "input",
         size = guiCoord(0, 5, 0, 5),
         position = guiCoord(0, 15, 0, 5),
         text = "...",
         backgroundAlpha = 0.75,
-        backgroundColour = colour:fromRGB0,0,0),
+        backgroundColour = colour:fromRGB(0,0,0)
     })
+
+    darkBtn:mouseLeftReleased(function()
+        themeController.set(themeController.darkTheme)
+    end)
+    
+    lightBtn:mouseLeftReleased(function()
+        themeController.set(themeController.lightTheme)
+    end)
+    
+    moreBtn:mouseLeftReleased(function()
+        print("Open main theme chooser")
+    end)
 end
-
-darkBtn:mouseLeftReleased(function()
-    themeController.set(themeController.darkTheme)
-end)
-
-lightBtn:mouseLeftReleased(function()
-    themeController.set(themeController.lightTheme)
-end)
-
-moreBtn.mouseLeftReleased(function()
-    print("Open main theme chooser")
-end)
 
 return controller
--- create/main.lua
+++ create/main.lua
@@ -4,16 +4,18 @@ return function(workshop)
 	local controllers = {
 		-- 1.0.0 has an internal console
 		--   = require("tevgit:create/controllers/console.lua"),
-		selection = require("tevgit:create/controllers/select.lua"),
-		theme     = require("tevgit:create/controllers/theme.lua"),
-		ui        = require("tevgit:create/controllers/ui.lua"),
-		camera    = require("tevgit:create/controllers/camera.lua"),
-		tool      = require("tevgit:create/controllers/tool.lua"),
-		property  = require("tevgit:create/controllers/propertyEditor.lua"),
-		hotkeys   = require("tevgit:create/controllers/hotkeys.lua")
+		selection 	  = require("tevgit:create/controllers/select.lua"),
+		theme     	  = require("tevgit:create/controllers/theme.lua"),
+		ui        	  = require("tevgit:create/controllers/ui.lua"),
+		camera    	  = require("tevgit:create/controllers/camera.lua"),
+		tool      	  = require("tevgit:create/controllers/tool.lua"),
+		property  	  = require("tevgit:create/controllers/propertyEditor.lua"),
+		hotkeys   	  = require("tevgit:create/controllers/hotkeys.lua"),
+		themeSwitcher = require("tevgit:create/controllers/themeSwitcher.lua")
 	}
 
 	controllers.ui.createMainInterface(workshop)
+	controllers.themeSwitcher.createUI(workshop)
 --	controllers.console.createConsole(workshop)
 	controllers.property.createUI(workshop)
 
