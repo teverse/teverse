c963c7a9ed90919a1d05d51b6e6bb97bc7e48af3
Jay <jay@teverse.com>
2019-08-29 00:20:47 +0100

    mocked up theme previewer

---
+++ workshop/controllers/ui/components/themePreviewer.lua
@@ -0,0 +1,44 @@
+-- Shows the range of colours of the current frame
+
+local ui = require("tevgit:workshop/controllers/ui/core/ui.lua")
+local shared = require("tevgit:workshop/controllers/shared.lua")
+
+local container = ui.create("guiFrame", shared.workshop, {
+   size = guiCoord(0.5, 0, 0.5, 0)
+}, "background")
+
+ui.create("guiTextBox", container, {
+   size = guiCoord(0.25, -10, 0.5, -10),
+   position = guiCoord(0, 5, 0, 5),
+   text = "Primary"
+}, "primary")
+
+ui.create("guiTextBox", container, {
+   size = guiCoord(0.25, -10, 0.5, -10),
+   position = guiCoord(0.25, 5, 0, 5),
+   text = "Primary Variant"
+}, "primaryVariant")
+
+ui.create("guiTextBox", container, {
+   size = guiCoord(0.25, -10, 0.5, -10),
+   position = guiCoord(0, 5, 0, 5),
+   text = "Secondary"
+}, "secondary")
+
+ui.create("guiTextBox", container, {
+   size = guiCoord(0.25, -10, 0.5, -10),
+   position = guiCoord(0.25, 5, 0, 5),
+   text = "Secondary Variant"
+}, "secondaryVariant")
+
+ui.create("guiTextBox", container, {
+   size = guiCoord(0.5, -10, 0.5, -10),
+   position = guiCoord(0, 5, 0.5, 5),
+   text = "Error"
+}, "error")
+
+ui.create("guiTextBox", container, {
+   size = guiCoord(0.5, -10, 0.5, -10),
+   position = guiCoord(0.5, 5, 0.5, 5),
+   text = "background"
+}, "background")
--- workshop/controllers/ui/themes/default.lua
+++ workshop/controllers/ui/themes/default.lua
@@ -3,9 +3,11 @@
 return {
     primary             = {
         backgroundColour = colour:fromRGB(99, 0, 238),
+        textColour = colour:fromRGB(255, 255, 255),
     },
     primaryVariant      = {
         backgroundColour = colour:fromRGB(55, 0, 179),
+        textColour = colour:fromRGB(255, 255, 255),
     },
     primaryText         = {
         textColour = colour:fromRGB(255, 255, 255),
@@ -13,9 +15,11 @@ return {
         
     secondary           = {
         backgroundColour = colour:fromRGB(3, 218, 198),
+        textColour = colour:fromRGB(0, 0, 0),
     },
     secondaryVariant    = {
         backgroundColour = colour:fromRGB(1, 134, 135),
+        textColour = colour:fromRGB(0, 0, 0),
     },
     secondaryText       = {
         textColour = colour:fromRGB(0, 0, 0),
@@ -23,6 +27,7 @@ return {
         
     error               = {
         backgroundColour = colour:fromRGB(176, 0, 32),
+        textColour = colour:fromRGB(255, 255, 255),
     },
     errorText           = {
         textColour = colour:fromRGB(255, 255, 255),
@@ -30,6 +35,7 @@ return {
         
     background          = {
         backgroundColour = colour:fromRGB(255, 255, 255),
+        textColour = colour:fromRGB(0, 0, 0),
     },
     backgroundText      = {
         textColour = colour:fromRGB(0, 0, 0),
