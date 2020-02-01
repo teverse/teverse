94c5c8fb92e0f7b35d7e59105183e6ea1b8eca42
Jay <jasoncarr@Jasons-MacBook-Pro.local>
2020-02-01 15:06:13 +0000

    0.17

--- core/client/loader.lua
+++ core/client/loader.lua
@@ -10,3 +10,12 @@ require("tevgit:core/client/debug.lua")
 require("tevgit:core/client/chat.lua")
 require("tevgit:core/client/playerList.lua")
 require("tevgit:core/client/characterController.lua")
+
+workspace.camera.position = vector3(40, 15, 0)
+workspace.camera:lookAt(vector3(0, 0, 0))
+
+engine.construct("block", workspace, {
+    position = vector3(0, 10, 0),
+    static = false,
+    colour = colour(0.3, 0.3, 0.3)
+})
