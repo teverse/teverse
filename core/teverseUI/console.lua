20b70ad8a7f70fdd98574826935a03f953f3f1c2
Jay <jasoncarr@Jasons-MacBook-Pro.local>
2020-04-17 22:09:32 +0100

    Console.lua

--- core/teverseUI/console.lua
+++ core/teverseUI/console.lua
@@ -1,11 +1,35 @@
 local container = teverse.construct("guiFrame", {
     parent = teverse.coreInterface,
-    size = guiCoord(0, 300, 0, 500),
+    size = guiCoord(0, 300, 0, 400),
     position = guiCoord(0, 20, 0, 20),
-    backgroundAlpha = 0.7,
+    backgroundAlpha = 0.9,
     zIndex = 1000,
     strokeRadius = 2,
+    strokeAlpha = 0.2,
     visible = false
 })
+
+teverse.construct("guiTextBox", {
+    parent = container,
+    size = guiCoord(1, -10, 0, 20),
+    position = guiCoord(0, 5, 0, 0),
+    backgroundAlpha = 0.0,
+    textSize = 16,
+    textAlign = "middleLeft",
+    text = "Console"
+})
+
+local txt = teverse.construct("guiTextBox", {
+    parent = container,
+    size = guiCoord(1, -10, 1, -25),
+    position = guiCoord(0, 5, 0, 20),
+    backgroundAlpha = 0.95,
+    strokeAlpha = 0.2,
+    textWrap = true
+})
+
+teverse.debug:on("print", function(msg)
+    txt.text = string.sub(os.date("%H:%M:%S") .. " : " .. msg .. "\n" .. txt.text, 0, 500)
+end)
 
 return container
