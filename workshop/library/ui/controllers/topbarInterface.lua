85f116e2716a14c6277fbcc2ce8d11ee13286593
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-05-02 20:24:25 -0400

    Fix naming error

--- workshop/library/ui/controllers/topbarInterface.lua
+++ workshop/library/ui/controllers/topbarInterface.lua
@@ -33,7 +33,7 @@ return {
             backgroundColour = globals.defaultColours.white,
         })
 
-        teverse.construct("guiImage", {
+        teverse.construct("guiIcon", {
             parent = container,
             size = guiCoord(0, 28, 0, 28),
             position = guiCoord(0.01, 0, 0.1, 0),
@@ -85,7 +85,7 @@ return {
             zIndex = 100
         })
 
-        local undoButton = teverse.construct("guiImage", {
+        local undoButton = teverse.construct("guiIcon", {
             parent = container,
             size = guiCoord(0, 20, 0, 20),
             position = guiCoord(0.92, 0, 0.2, 0),
@@ -95,7 +95,7 @@ return {
             backgroundColour = globals.defaultColours.white,
         })
 
-        local redoButton = teverse.construct("guiImage", {
+        local redoButton = teverse.construct("guiIcon", {
             parent = container,
             size = guiCoord(0, 20, 0, 20),
             position = guiCoord(0.94, 0, 0.2, 0),
@@ -105,7 +105,7 @@ return {
             backgroundColour = globals.defaultColours.white,
         })
 
-        local settingsButton = teverse.construct("guiImage", {
+        local settingsButton = teverse.construct("guiIcon", {
             parent = container,
             size = guiCoord(0, 20, 0, 20),
             position = guiCoord(0.97, 0, 0.2, 0),
