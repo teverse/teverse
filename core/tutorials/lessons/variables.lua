a02461490228adb3848347c6ae2acd6780a40f89
rgold18 <34944924+rgold18@users.noreply.github.com>
2020-05-05 15:41:50 -0700

    Reformatted code to be uniform

--- core/tutorials/lessons/variables.lua
+++ core/tutorials/lessons/variables.lua
@@ -4,44 +4,36 @@ return {
     name = "Variables",
     description = "Learning Variables",
     pages = {
-        framework.interactiveCode(
-            "Naming things in our world ", 
+        framework.interactiveCode("Naming things in our world ",
             [[Let's jump straight into creating your first variables!
-            
-Type testVariable between the two quotation marks, then press run]],
-            [[local newVariable = "blank"]],
-            function(script, logs)
-                local correct = script.text:lower() == "local newvariable = \"testvariable\""
-                if not correct then
-                    print("Try again thats not quite right...\n")
-                else
-                    local newVariable = "testVariable"
-                    print("Well done! Press next!\n")
-                    print(newVariable)
-                end
-                return correct
-            end
-        ),
-        framework.exampleCode(
-            "Dissecting your script",
-[[A variable is a way to assign a name to something.
-Variables can come in the form of multiple types: strings, numbers, booleans, tables, functions, and much more!
-
-You declared the variable "newVariable" as a string "testVariable"; a string is usually a phrase or non-numeric set of characters.
-
-The print function simply outputs the stuff you give it to the console. The console is what you seen on the right of the screen.
-
-Example Output from the script on the left:]],
-[[local newPhrase = "This is a string"
-local newNumber = 4
-
-print(newPhrase)
-print(newNumber)
-
-]],
-[["This is a string"
-4
-]]
-        )
-    }
-}
+            Type testVariable between the two quotation marks, then press run]],
+                [[local newVariable = "blank"]],
+                    function(script, logs)
+                        local correct = script.text:lower() == "local newvariable = \"testvariable\""
+                        if not correct then
+                            print("Try again thats not quite right...\n")
+                        else
+                        local newVariable = "testVariable"
+                            print("Well done! Press next!\n")
+                            print(newVariable)
+                        end
+                        return correct
+                    end),
+                    framework.exampleCode("Dissecting your script",
+                        [[A variable is a way to assign a name to something.
+                        Variables can come in the form of multiple types: strings, numbers, booleans, tables, functions, and much more!
+                        In the first section, you declared the variable "newVariable" as a string "testVariable";a string is usually a phrase or non-numeric set of characters.
+                        The print function simply outputs the stuff you give it to the console. The console is what you seen on the right of the screen.
+                        Example Output from the script on the left:]],
+                            [[
+                                local newPhrase = "This is a string"
+                                local newNumber = 4
+                                print(newPhrase)
+                                print(newNumber)
+                            ]], 
+                            [[
+                                "This is a string"
+                                4
+                            ]])
+                        }
+                    }
