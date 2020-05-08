2da7b377effb1bf9e6e56b4d38ab39fcd63f6031
teverse <jay@teverse.com>
2020-05-08 20:11:32 +0100

    added console history

--- core/teverseUI/console.lua
+++ core/teverseUI/console.lua
@@ -28,6 +28,14 @@ local txt = teverse.construct("guiTextBox", {
     textWrap = true
 })
 
+if _TEV_VERSION_PATCH and _TEV_VERSION_PATCH >= 9 then
+    for _,v in pairs(teverse.debug:getOutputHistory()) do
+        txt.text = txt.text .. "\n" .. os.date("%H:%M:%S (h)", v.time) .. " : " .. v.message
+    end
+else
+    print("History not supported")
+end
+
 teverse.debug:on("print", function(msg)
     txt.text = string.sub(os.date("%H:%M:%S") .. " : " .. msg .. "\n" .. txt.text, 0, 500)
 end)
