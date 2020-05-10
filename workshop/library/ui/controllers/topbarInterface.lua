dc2577fb8b85d5f0df8510fa03443688827ff674
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-05-10 15:31:22 -0400

    Correct vsc merge error

--- workshop/library/ui/controllers/topbarInterface.lua
+++ workshop/library/ui/controllers/topbarInterface.lua
@@ -33,7 +33,7 @@ return {
             backgroundColour = globals.defaultColours.white,
         })
 
-        teverse.construct("guiImage", {
+        teverse.construct("guiIcon", {
             parent = container,
             size = guiCoord(0, 28, 0, 28),
             position = guiCoord(0.01, 0, 0.1, 0),
@@ -72,7 +72,7 @@ return {
         })
 
         local statusIcon = teverse.construct("guiFrame", {
-            parent = container
+            parent = container,
             size = guiCoord(0, 16, 0, 16),
             position = guiCoord(0.836, 0, 0.5, 0),
             backgroundColour = globals.defaultColours.green,
@@ -83,7 +83,7 @@ return {
             zIndex = 100
         })
 
-        local undoButton = teverse.construct("guiImage", {
+        local undoButton = teverse.construct("guiIcon", {
             parent = container,
             size = guiCoord(0, 20, 0, 20),
             position = guiCoord(0.92, 0, 0.2, 0),
@@ -93,7 +93,7 @@ return {
             backgroundColour = globals.defaultColours.white,
         })
 
-        local redoButton = teverse.construct("guiImage", {
+        local redoButton = teverse.construct("guiIcon", {
             parent = container,
             size = guiCoord(0, 20, 0, 20),
             position = guiCoord(0.94, 0, 0.2, 0),
@@ -103,7 +103,7 @@ return {
             backgroundColour = globals.defaultColours.white,
         })
 
-        local settingsButton = teverse.construct("guiImage", {
+        local settingsButton = teverse.construct("guiIcon", {
             parent = container,
             size = guiCoord(0, 20, 0, 20),
             position = guiCoord(0.97, 0, 0.2, 0),
