c4e0a5be6764bbd5e35c41c74a7a5b8f3118cdaa
Sanjay <Sanjay-B@users.noreply.github.com>
2020-06-15 13:05:57 -0400

    TevX - Workshop Revamp (#76)
    * Refactor .parent change in teverse.construct
    
    * Redesign to fit dashboard screen

--- workshop/library/globals.lua
+++ workshop/library/globals.lua
@@ -8,7 +8,8 @@ return {
     sideBarPageDefault = nil, -- Holds the default sidebar page (view) as a string and is set in topbarInterface.lua
     sideBarPageActive = nil, -- Holds the current active sidebar page (view) as a string and is set in topbarInterface.lua
     defaultColours = { -- Default colors used for theming UI components (~\library\ui\components)
-        primary = colour.rgb(112, 112, 112),
+        --primary = colour.rgb(112, 112, 112),
+        primary = colour.rgb(52, 58, 64),
         secondary = colour.rgb(239, 239, 239),
         background = colour.rgb(33, 33, 33),
         red = colour.rgb(255, 82, 82),
@@ -17,6 +18,7 @@ return {
         blue = colour.rgb(68, 138, 255),
         orange = colour.rgb(255, 171, 64),
         purple = colour.rgb(124, 77, 255),
-        white = colour.rgb(255, 255, 255)
+        white = colour.rgb(255, 255, 255),
+        black = colour(0, 0, 0)
     }
 }
--- workshop/library/ui/controllers/workshopInterface.lua
+++ workshop/library/ui/controllers/workshopInterface.lua
@@ -2,54 +2,69 @@
 -- This script is responsible for creating the workshop interface
 
 local globals = require("tevgit:workshop/library/globals.lua") -- globals; variables or instances that can be shared between files
---local prompt = require("tevgit:workshop/library/ui/components/prompt.lua") -- UI Component
 local topbarInterface = require("tevgit:workshop/library/ui/controllers/topbarInterface.lua") -- UI Controller
 local sidebarInterface = require("tevgit:workshop/library/ui/controllers/sidebarInterface.lua") -- UI Controller
 
-local topBar = topbarInterface.construct("horizontalNavbar", "cloud", "Test Place 1") -- Create initial topbar instance
+local topBar = topbarInterface.construct("horizontalNavbar", "Test Place 1") -- Create initial topbar instance
 local sideBar = sidebarInterface.construct("verticalNavbar") -- Create initial sidebar instance
 
+-- Register default tools to topbar instance
+topBar.registerIcon("sliders-h", nil)
+topBar.registerIcon("arrow-right", nil)
+topBar.registerIcon("arrow-left", nil)
+
+-- Register default tools to sidebar instance
+sideBar.registerDefaultIcon("location-arrow", nil)
+sideBar.registerDefaultIcon("arrows-alt-h", nil)
+sideBar.registerDefaultIcon("sync", nil)
+sideBar.registerDefaultIcon("expand", nil)
+
 local defaultPage = sideBar.registerPage("Default") -- Register default page to sidebar instance
-sideBar.registerIcon(defaultPage, "openFolderIcon", "folder-open", "Open Folder", nil) 
-sideBar.registerIcon(defaultPage, "newFileIcon", "file", "Create new file", nil)
-sideBar.registerIcon(defaultPage, "uploadFileIcon", "file-upload", "Upload current file", nil)
-sideBar.registerIcon(defaultPage, "downloadFileIcon", "file-download", "Download current file", nil)
-sideBar.registerIcon(defaultPage, "importFileIcon", "file-import", "Import a file", nil, -0.048, guiCoord(0.048, 0, 0, 0))
-sideBar.registerIcon(defaultPage, "exportFileIcon", "file-export", "Export current file", nil, 0.048, guiCoord(-0.048, 0, 0, 0))
-defaultPage.visible = true -- Set default sidebar page to visible
-globals.sideBarPageDefault = defaultPage -- Set default sidebar page to default
-globals.sideBarPageActive = defaultPage -- Set default sidebar page as active
+defaultPage.registerIcon("openFolderIcon", "folder-open", nil)
+defaultPage.registerIcon("newFileIcon", "file", nil)
+defaultPage.registerIcon("uploadFileIcon", "file-upload", nil)
+defaultPage.registerIcon("downloadFileIcon", "file-download", nil)
+defaultPage.registerIcon("importFileIcon", "file-import", nil)
+defaultPage.registerIcon("exportFileIcon", "file-export", nil)
+--globals.sideBarPageDefault = defaultPage -- Set default sidebar page to default
+--globals.sideBarPageActive = defaultPage -- Set default sidebar page as active
 
-
-local designPage = sideBar.registerPage("Design") -- Design default page to sidebar instance
-sideBar.registerIcon(designPage, "screenContainerIcon", "tv", "Create a container instance", nil)
-sideBar.registerIcon(designPage, "guiFrameIcon", "square-full", "Create a guiFrame instance", nil)
-sideBar.registerIcon(designPage, "guiTextBoxIcon", "i-cursor", "Create a guiTextBox instance", nil)
-sideBar.registerIcon(designPage, "guiImageIcon", "image", "Create a guiImage instance", nil)
-
+local designPage = sideBar.registerPage("Design") -- Register design page to sidebar instance
+designPage.registerIcon("screenContainerIcon", "tv", nil)
+designPage.registerIcon("guiFrameIcon", "square-full", nil)
+designPage.registerIcon("guiTextBoxIcon", "i-cursor", nil)
+designPage.registerIcon("guiImageIcon", "image", nil)
 
 local modelPage = sideBar.registerPage("Model") -- Register model page to sidebar instance
-sideBar.registerIcon(modelPage, "modelIcon", "shapes", "Group instance(s) together", nil)
-
+modelPage.registerIcon("modelIcon", "shapes", nil)
 
 local insertPage = sideBar.registerPage("Insert") -- Register insert page to sidebar instance
-sideBar.registerIcon(insertPage, "blockIcon", "cube", "Create a cube instance", nil)
-sideBar.registerIcon(insertPage, "circleIcon", "circle", "Create a cylinder instance", nil)
---sideBar.registerIcon(designPage, "triangleIcon", "i-cursor", nil, nil) -- Triangle / Wedge Icon doesn't exist
-sideBar.registerIcon(insertPage, "scriptIcon", "code", "Create a script instance", nil)
-sideBar.registerIcon(insertPage, "lightIcon", "lightbulb", "Create a light instance", nil)
-
+insertPage.registerIcon("blockIcon", "cube", nil)
+insertPage.registerIcon("circleIcon", "circle", nil)
+insertPage.registerIcon("scriptIcon", "code", nil)
+insertPage.registerIcon("lightIcon", "lightbulb", nil)
 
 local testPage = sideBar.registerPage("Test") -- Register test page to sidebar instance
-sideBar.registerIcon(testPage, "consoleIcon", "terminal", " Open console window", nil)
-sideBar.registerIcon(testPage, "playIcon", "play", "Play scene", nil)
-sideBar.registerIcon(testPage, "serverIcon", "server", "Configure server", nil)
-sideBar.registerIcon(testPage, "fullScreenIcon", "expand-alt", "Toggle full screen", nil)
+testPage.registerIcon("consoleIcon", "terminal", nil)
+testPage.registerIcon("playIcon", "play", nil)
+testPage.registerIcon("serverIcon", "server", nil)
+testPage.registerIcon("fullScreenIcon", "expand-alt", nil)
+
+-- Bind pages to labels in menu
+topBar.bindDefaultMenu(defaultPage.getContainer())
+topBar.bindMenu("Design", designPage.getContainer())
+topBar.bindMenu("Model", modelPage.getContainer())
+topBar.bindMenu("Insert", insertPage.getContainer())
+topBar.bindMenu("Test", testPage.getContainer())
 
 
 
+
+
+--[[
 -- Register topbar button (name labels) to topbar instance
 topBar.register("Design", "Design your guis", designPage)
 topBar.register("Model", "Model your scene", modelPage)
 topBar.register("Insert", "Insert an instance to your scene", insertPage)
 topBar.register("Test", "Test your scene", testPage)
+]]--
