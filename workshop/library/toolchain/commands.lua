29d3d912451be369b0e3fd66ad116360da5a3f44
Sanjay Bhadra <sanjaybhadra1821@gmail.com>
2020-07-03 12:32:22 -0400

    File cleanup

--- workshop/library/toolchain/commands.lua
+++ workshop/library/toolchain/commands.lua
@@ -3,27 +3,6 @@
 
 local globals = require("tevgit:workshop/library/globals.lua") -- globals; variables or instances that can be shared between files
 
---[[
-    TEST DOCUMENT
-
-    -- How to register a Command / Extension
-
-    local commands = require("...")
-    local extension = commands.createGroup("Hello")
-    extension.command("ping", function() print("pong") end)
-    -- >hello: ping
-    ---> pong!
-
-    -- Command Structure
-    {
-        [group] = {
-            [index] = {
-                [namespace] = function 0xNum
-            }
-        }
-    }
-]]
-
 function dump(o)
     if type(o) == 'table' then
        local s = '{ '
@@ -44,7 +23,6 @@ return {
         self = data
         self.id = id
         self.commands = {}
-        --table.insert(globals.commandGroups, {id={}})
         globals.commandGroups[id] = {}
 
         self.command = function(id, callback)
@@ -61,13 +39,6 @@ return {
         if commandIndex == -1 or commandIndex == nil then print("Failed to find command group: "..text) return end
         local commandGroup = string.gsub(string.sub(text, 0, (commandIndex-1)), "%s+", "")
         local commandName = string.gsub(string.sub(text, (commandIndex+1), string.len(text)), "%s+", "")
-        --print("commandGroup: "..commandGroup)
-        --print("commandName: "..commandName)
-        --print("globalCommandsCount: "..#globals.commandGroups)
-
-        --string.gsub(str, "%s+", "")
-
-        --print(dump(globals.commandGroups))
 
         for row, group in pairs(globals.commandGroups) do
             for column, placement in pairs(group) do
